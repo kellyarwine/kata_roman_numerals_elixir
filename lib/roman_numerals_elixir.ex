43eaec3284c2b435d6779d79fa3f8db16d97d6fe
Kelly Steensma <kellysteensma@gmail.com>
2013-04-24 14:55:17 -0500

    Adding code to pass basic lookup tests.

--- lib/roman_numerals_elixir.ex
+++ lib/roman_numerals_elixir.ex
@@ -1,20 +1,23 @@
 defmodule RomanNumeralsElixir do
 
   def convert_number(number) do
-    "I"
+    lookup_number(number, [[10,"X"], [5,"V"], [1,"I"]], [])
   end
 
-  # def` convert_number(number) do
-  #   lookup_number(number, [["10","X"], ["5" "V", 1: "I"], [])
-  # end
+  def lookup_number(number, [current_lookup | remaining_lookups], acc) do
+    lower_method(number, current_lookup, remaining_lookups, acc)
+  end
 
-  # def lookup_number(number, [current_lookup | remaining_lookup], acc) do
-  #   if number == current_numeral do
-  #     roman_numeral = Keyword.get_values current_lookup
-  #     lookup_number(number = number - , roman_numeral ++ acc
-  #   else
-  #     "V"
-  #   end
+  def lower_method(number, [current_number| current_numeral], remaining_lookups, acc) do
+    if number == current_number do
+      list_to_bitstring([current_numeral] ++ acc)
+    else
+      lookup_number(number, remaining_lookups, acc)
+    end
+  end
+
+  # def lookup_number(number, [], acc) do
+  #   acc
   # end
 
 end
