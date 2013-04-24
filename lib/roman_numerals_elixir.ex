b810521a973e49e16a0cbdc23d371b7d73b67b98
Kelly Steensma <kellysteensma@gmail.com>
2013-04-24 15:53:50 -0500

    Adding full code base and all tests.

--- lib/roman_numerals_elixir.ex
+++ lib/roman_numerals_elixir.ex
@@ -1,23 +1,50 @@
 defmodule RomanNumeralsElixir do
+  # VARIABLE = 1
+
+  def roman_numeral_lookup do
+  [
+    [1000,  "M"],
+    [900 , "CM"],
+    [500 ,  "D"],
+    [100 ,  "C"],
+    [90  , "XC"],
+    [50  ,  "L"],
+    [40  , "XL"],
+    [10  ,  "X"],
+    [9   , "IX"],
+    [5   ,  "V"],
+    [4   , "IV"],
+    [1   ,  "I"],
+    [40  , "IV"],
+    [10  ,  "X"],
+    [5   ,  "V"],
+    [1   ,  "I"],
+  ]
+  end
 
   def convert_number(number) do
-    lookup_number(number, [[10,"X"], [5,"V"], [1,"I"]], [])
+    parse_lookup(number, roman_numeral_lookup, [])
   end
 
-  def lookup_number(number, [current_lookup | remaining_lookups], acc) do
-    lower_method(number, current_lookup, remaining_lookups, acc)
-  end
-
-  def lower_method(number, [current_number| current_numeral], remaining_lookups, acc) do
-    if number == current_number do
-      list_to_bitstring([current_numeral] ++ acc)
+  def parse_lookup(number, [current_lookup | remaining_lookups], acc) do
+    if number > 0 do
+      roman_numeral(number, current_lookup, remaining_lookups, acc)
     else
-      lookup_number(number, remaining_lookups, acc)
+      list_to_bitstring(acc)
     end
   end
 
-  # def lookup_number(number, [], acc) do
-  #   acc
-  # end
+  def roman_numeral(number, [current_number | current_numeral], remaining_lookups, acc) do
+    if number >= current_number do #) == 0 && number > 0 do
+    # IO.puts "number: #{number} | current_number: #{current_number} | remaining: #{rem(number, current_number)} | acc: #{acc}"
+      roman_numeral(number - current_number, [current_number | current_numeral], remaining_lookups, acc ++ [current_numeral])
+    else
+      parse_lookup(number, remaining_lookups, acc)
+    end
+  end
+
+  def parse_lookup(number, [], acc) do
+    list_to_bitstring(acc)
+  end
 
 end
--- test/roman_numerals_elixir_test.exs
+++ test/roman_numerals_elixir_test.exs
@@ -14,4 +14,85 @@ defmodule RomanNumeralsElixirTest do
   test "returns X when 10 is input" do
     assert(RomanNumeralsElixir.convert_number(10) == "X")
   end
+
+  test "returns IV when 4 is input" do
+    assert(RomanNumeralsElixir.convert_number(4) == "IV")
+  end
+
+  test "returns M when 1000 is input" do
+    assert(RomanNumeralsElixir.convert_number(1000) == "M")
+  end
+
+  test "returns II when 2 is input" do
+    assert(RomanNumeralsElixir.convert_number(2) == "II")
+  end
+
+  test "returns III when 3 is input" do
+    assert(RomanNumeralsElixir.convert_number(3) == "III")
+  end
+
+  test "returns VI when 6 is input" do
+    assert(RomanNumeralsElixir.convert_number(6) == "VI")
+  end
+
+  test "returns IX when 9 is input" do
+    assert(RomanNumeralsElixir.convert_number(9) == "IX")
+  end
+
+  test "returns XI when 11 is input" do
+    assert(RomanNumeralsElixir.convert_number(11) == "XI")
+  end
+
+  test "returns XIV when 14 is input" do
+    assert(RomanNumeralsElixir.convert_number(14) == "XIV")
+  end
+
+  test "returns XV when 15 is input" do
+    assert(RomanNumeralsElixir.convert_number(15) == "XV")
+  end
+
+  test "returns XIX when 19 is input" do
+    assert(RomanNumeralsElixir.convert_number(19) == "XIX")
+  end
+
+  test "returns XL when 40 is input" do
+    assert(RomanNumeralsElixir.convert_number(40) == "XL")
+  end
+
+  test "returns XC when 90 is input" do
+    assert(RomanNumeralsElixir.convert_number(90) == "XC")
+  end
+
+  test "returns XCIX when 99 is input" do
+    assert(RomanNumeralsElixir.convert_number(99) == "XCIX")
+  end
+
+  test "returns DI when 501 is input" do
+    assert(RomanNumeralsElixir.convert_number(501) == "DI")
+  end
+
+  test "returns DL when 550 is input" do
+    assert(RomanNumeralsElixir.convert_number(550) == "DL")
+  end
+
+  test "returns DCCCXC when 890 is input" do
+    assert(RomanNumeralsElixir.convert_number(890) == "DCCCXC")
+  end
+
+  test "returns MDCCC when 1800 is input" do
+    assert(RomanNumeralsElixir.convert_number(1800) == "MDCCC")
+  end
+
+  test "returns CM when 900 is input" do
+    assert(RomanNumeralsElixir.convert_number(900) == "CM")
+  end
+
+  test "returns CMXC when 990 is input" do
+    assert(RomanNumeralsElixir.convert_number(990) == "CMXC")
+  end
+
+  test "returns MMMMM when 5000 is input" do
+    assert(RomanNumeralsElixir.convert_number(5000) == "MMMMM")
+  end
+
 end
